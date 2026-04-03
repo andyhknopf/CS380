36393a4e9d283df2c96423926be0e55ec1a3999f
andyhknopf <andy.h.knopf@gmail.com>
2026-04-02 20:46:43 -0700

    Research Project Initial push test
    Testing initial push with .gitignore

---
+++ CS380-ResearchProject/Assets/AndyScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AndyScript : MonoBehaviour
+{
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.A))
+      Debug.Log("Andy");
+  }
+}
